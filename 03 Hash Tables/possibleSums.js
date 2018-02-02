361a37591ec574ad4b9c396416a0a41be96b4e4f
RudyRed <willgross94@gmail.com>
2018-02-01 19:25:03 -0500

    4/5 Hash Table Questions Completed

---
+++ 03 Hash Tables/possibleSums.js
@@ -0,0 +1,74 @@
+// You have a collection of coins, and you know the values of the coins and the
+// quantity of each type of coin in it. You want to know how many distinct sums
+// you can make from non-empty groupings of these coins.
+//
+// Example
+//
+// For coins = [10, 50, 100] and quantity = [1, 2, 1], the output should be
+// possibleSums(coins, quantity) = 9.
+//
+// Here are all the possible sums:
+//
+// 50 = 50;
+// 10 + 50 = 60;
+// 50 + 100 = 150;
+// 10 + 50 + 100 = 160;
+// 50 + 50 = 100;
+// 10 + 50 + 50 = 110;
+// 50 + 50 + 100 = 200;
+// 10 + 50 + 50 + 100 = 210;
+// 10 = 10;
+// 100 = 100;
+// 10 + 100 = 110.
+// As you can see, there are 9 distinct sums that can be created from non-empty groupings of your coins.
+//
+// Input/Output
+//
+// [execution time limit] 4 seconds (js)
+//
+// [input] array.integer coins
+//
+// An array containing the values of the coins in your collection.
+//
+// Guaranteed constraints:
+// 1 ≤ coins.length ≤ 20,
+// 1 ≤ coins[i] ≤ 104.
+//
+// [input] array.integer quantity
+//
+// An array containing the quantity of each type of coin in your collection.
+// quantity[i] indicates the number of coins that have a value of coins[i].
+//
+// Guaranteed constraints:
+// quantity.length = coins.length,
+// 1 ≤ quantity[i] ≤ 105.
+//
+// It is guaranteed that (quantity[0] + 1) * (quantity[1] + 1) * ... * (quantity[quantity.length - 1] + 1) <= 106.
+//
+// [output] integer
+//
+// The number of different possible sums that can be created from non-empty groupings of your coins.
+
+const possibleSums = function (coins, quantity, recursiveCall = false) {
+
+  let sums = {0: true};
+
+  while (coins.length) {
+    const coinVal = coins.pop();
+    const coinQuant = quantity.pop();
+    const res = possibleSums(coins, quantity, true);
+    for (let numOfCurrCoin = 0; numOfCurrCoin <= coinQuant; numOfCurrCoin++) {
+      for (let sum in res) {
+        sums[Number(sum) + coinVal * numOfCurrCoin] = true;
+      }
+    }
+  }
+
+  if (recursiveCall) {
+    return sums;
+  }
+
+  return Object.keys(sums).length - 1;
+};
+
+// console.log(possibleSums([10, 50, 100, 500], [5, 3, 2, 2]), 's/b 9')
